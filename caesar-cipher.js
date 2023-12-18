f3e87097956a0f9c9427a2e6318ff214583a1c82
Don-Puffin <152089547+Don-Puffin@users.noreply.github.com>
2023-12-18 15:04:45 +0000

    secondary cipher implemented

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,40 +1,32 @@
-// Function to encrypt the input text using the Caesar Cipher
 function encrypt() {
-  // Get the shift value from the input field
-  const shift = parseInt(document.getElementById("shift").value);
-  // Get the input text from the textarea
-  const inputText = document.getElementById("inputText").value;
-  // Set the encrypted result in the output textarea
-  document.getElementById("outputText").value = caesarCipher(inputText, shift);
+  const primaryShift = parseInt(document.getElementById('primaryShift').value);
+  const secondaryShift = parseInt(document.getElementById('secondaryShift').value);
+  const inputText = document.getElementById('inputText').value;
+  document.getElementById('outputText').value = doubleShiftCipher(inputText, primaryShift, secondaryShift);
 }
 
-// Function to decrypt the input text using the Caesar Cipher
 function decrypt() {
-  // Get the shift value from the input field
-  const shift = parseInt(document.getElementById("shift").value);
-  // Get the input text from the textarea
-  const inputText = document.getElementById("inputText").value;
-  // Set the decrypted result in the output textarea
-  document.getElementById("outputText").value = caesarCipher(inputText, -shift); // Note: Use -shift to reverse the encryption
+  const primaryShift = parseInt(document.getElementById('primaryShift').value);
+  const secondaryShift = parseInt(document.getElementById('secondaryShift').value);
+  const inputText = document.getElementById('inputText').value;
+  document.getElementById('outputText').value = doubleShiftCipher(inputText, -primaryShift, -secondaryShift);
 }
 
-// Function to perform the Caesar Cipher encryption/decryption
-function caesarCipher(text, shift) {
-  return text
-    .split("")
-    .map((char) => {
-      if (char.match(/[a-z]/i)) {
-        const code = char.charCodeAt(0);
-        const offset = code < 91 ? 65 : 97;
-        return String.fromCharCode(
-          ((code - offset + shift + 26) % 26) + offset
-        );
-      } else {
-        const charCode = char.charCodeAt(0);
-        return String.fromCharCode((charCode + shift + 256) % 256);
-      }
-    })
-    .join("");
+function doubleShiftCipher(text, primaryShift, secondaryShift) {
+  return text.split('').map(char => {
+    if (char.match(/[a-z]/i)) {
+      const code = char.charCodeAt(0);
+      const offset = code < 91 ? 65 : 97;
+      const primaryShifted = (code - offset + primaryShift + 26) % 26 + offset;
+      const secondaryShifted = (primaryShifted - offset + secondaryShift + 26) % 26 + offset;
+      return String.fromCharCode(secondaryShifted);
+    } else {
+      const charCode = char.charCodeAt(0);
+      return String.fromCharCode((charCode + primaryShift + secondaryShift) % 256);
+    }
+  }).join('');
 }
+
+
 
 module.exports = { caesarCipher };
